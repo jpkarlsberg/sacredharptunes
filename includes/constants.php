e3a75d90290f5ceb6bca7291de906e7b96e665ef
Jesse P. Karlsberg <jesse@jpkarlsberg.com>
2018-10-18 18:08:05 -0400

    Add files via upload

---
+++ includes/constants.php
@@ -0,0 +1,39 @@
+<?php
+
+	/* connnection variables */
+	define("DB_SERVER", "localhost");
+	define("DB_USER", "REDACTED");
+	define("DB_PASS", "REDACTED");
+	define("DB_NAME", "REDACTED");
+	
+	/* name variables */
+	define("ADMIN_NAME", "Jesse Pearlman Karlsberg");
+	define("ADMIN_NICKNAME", "Jesse");
+	define("ADMIN_EMAIL", "jesse@silversand.org");
+	
+	/* site variables */
+	define("SITE_ROOT_PATH", "/home/silver/");
+	define("SITE_ROOT_HTML_PATH", "/home/silver/public_html/sacredharptunes/");
+	define("SITE_NAME_PUBLIC", "New Compositions in the Style of the Sacred Harp");
+	define("SITE_NAME_SHORT", "New Sacred Harp Tunes");
+	define("SITE_NAME_PRIVATE", "Sacred Harp Tunes Organizer");
+	define("SITE_NAME_SHORT_PRIVATE", "Sacred Harp Tunes");
+	define("SITE_ABBR", "SHTO");
+	define("SITE_URL", "http://sacredharptunes.com/");
+	define("SITE_INCLUDES_PATH", "includes/");
+	define("SITE_LOGIN_PATH", "organizer/");
+	define("SITE_FILES_URL", "http://media.sacredharptunes.com/");
+	
+	/* database variables */
+	define("TBL_TUNES", "tunes");
+	
+	/* AWS variables */
+	define("AWS_ACCESS_KEY", "REDACTED");
+	define("AWS_SECRET_KEY", "REDACTED");
+	define("S3_BUCKET", "REDACTED");
+	
+	define("PDF_VIEWER_CODE", "");
+	//define("PDF_VIEWER_CODE", "http://docs.google.com/viewer?url=");
+	//define("TARGET_CODE", "");
+	define("TARGET_CODE", " target=\"blank\"");
+?>
