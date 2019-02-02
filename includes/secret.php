6c51866493c55b2e3eb1eaae7ba87c6ac47c2f13
Steve <steve.brett.design@gmail.com>
2019-02-02 23:11:56 +0000

    Conceal confidential settings

---
+++ includes/secret.php
@@ -0,0 +1,14 @@
+<?php
+
+/* connnection variables */
+define("DB_SERVER", "localhost");
+define("DB_USER", "REDACTED");
+define("DB_PASS", "REDACTED");
+define("DB_NAME", "REDACTED");
+
+/* AWS variables */
+define("AWS_ACCESS_KEY", "REDACTED");
+define("AWS_SECRET_KEY", "REDACTED");
+define("S3_BUCKET", "REDACTED");
+
+ ?>
