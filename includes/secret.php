dbe558a25d97a3f5b844c19f30932932048b4921
jpkarlsberg <jesse@jpkarlsberg.com>
2019-03-04 15:51:03 -0500

    AWS variables

--- includes/secret.php
+++ includes/secret.php
@@ -7,8 +7,8 @@ define("DB_PASS", "dyingcalifornian410t");
 define("DB_NAME", "silver_tunes");
 
 /* AWS variables */
-define("AWS_ACCESS_KEY", "REDACTED");
-define("AWS_SECRET_KEY", "REDACTED");
-define("S3_BUCKET", "REDACTED");
+define("AWS_ACCESS_KEY", "AKIAJWL76O7RCUNORE5A");
+define("AWS_SECRET_KEY", "tBL+9PyhCFGFJonLfh0d0hpGRXIUKDSICCRu9V86");
+define("S3_BUCKET", "media.sacredharptunes.com");
 
  ?>
